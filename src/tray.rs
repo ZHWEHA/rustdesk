c9d64cdaa998b78f173fb59d32e4151674e35038
ZHWEHA <zhweha00@outlook.com>
2025-12-03 23:14:18 +0800

    Implement body for make_tray and load_icon_from_asset

--- src/tray.rs
+++ src/tray.rs
@@ -10,10 +10,16 @@ use std::time::Duration;
 
 pub fn start_tray() {}
 
-fn make_tray() -> hbb_common::ResultType<()> {}
+fn make_tray() -> hbb_common::ResultType<()> {
+    Ok(())
+}
 
 #[cfg(windows)]
 #[tokio::main(flavor = "current_thread")]
-async fn start_query_session_count(sender: std::sync::mpsc::Sender<Data>) {}
+async fn start_query_session_count(sender: std::sync::mpsc::Sender<Data>) {
+}
 
-fn load_icon_from_asset() -> Option<image::DynamicImage> {}
+fn load_icon_from_asset() -> Option<image::DynamicImage> {
+
+    None
+}
